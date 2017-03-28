189a4da89f4a2e2d86078dc97dcce75b1bc6bc11
ga-wolf <jack.jeffress@ga.co>
2017-03-28 15:10:00 +1100

    Added Slides from today

---
+++ 01-javascript/primitives_and_variables/main.js
@@ -0,0 +1,63 @@
+// // Primitive Data Types
+//   // Immutable
+//   // Basic building blocks of the language
+//
+// // Strings
+// console.log( "Hello World" );
+// console.log( 'It\'s an orange' );
+// console.log( "It's an orange" );
+// // Concatenation is combining two strings
+// console.log( "Hello " + "Jane" );
+// console.log( "Hello".length );
+// console.log( "Hello".toUpperCase() );
+// console.log( "Hello".endsWith("!") );
+//
+// // Numbers
+//   // Floats and Integers
+//
+// // BOMDAS (Order of Operations)
+//   // Brackets
+//   // Orders (Exponents)
+//   // Multiplication
+//   // Division
+//   // Addition
+//   // Subtraction
+// console.log( 4 );
+// console.log( 1209412940 );
+// console.log( -0.52 );
+// console.log( 4 + 8 );
+// console.log( 4 - 8 );
+// console.log( 4 * 12 ); // Multiplication
+// console.log( 4 / 12 ); // Division
+// // Modulus or Modulo Operator (Remainder Operator)
+// console.log( 5 % 3 );
+// console.log( 4 % 3 );
+// console.log( 0.3 - 0.2 );
+//
+// // Booleans
+//   // True or false (the decision makers)
+// console.log( true );
+// console.log( false );
+//
+// // Null - Explicitly Empty
+//   // You set this
+// console.log( null );
+//
+// // Undefined - Implicitly Empty
+//   // You let the browser set this
+// console.log( undefined );
+
+// VARIABLES
+  // Places in memory that are given a name
+
+var favNumber = 21;
+console.log( favNumber );
+
+favNumber = 42;
+console.log( "favNumber is now:", favNumber );
+
+var name = "Kane";
+var greeting = "Hello " + name;
+var excitedGreeting = greeting + "!";
+
+var otherExcitedGreeting = "Hello " + name + "!";
