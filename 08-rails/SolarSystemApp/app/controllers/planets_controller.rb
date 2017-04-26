497ffea83455dac207130b2c68bd2df2b0b9ce39
ga-wolf <jack.jeffress@ga.co>
2017-04-26 15:54:43 +1000

    Finished Solar System app

--- 08-rails/SolarSystemApp/app/controllers/planets_controller.rb
+++ 08-rails/SolarSystemApp/app/controllers/planets_controller.rb
@@ -12,6 +12,38 @@ class PlanetsController < ApplicationController
   def new
   end
 
-  def edit
+  def create
+    planet = Planet.create( planet_params() )
+    redirect_to "/planets"
   end
+
+  def edit
+    id = params["id"]
+    @planet = Planet.find_by(id: id)
+  end
+
+  def update
+    # I need to find the planet
+    planet = Planet.find_by(id: params["id"])
+    # I need to update the planet using strong params
+    planet.update( planet_params )
+    # I need to redirect to /planets/:id
+    redirect_to "/planets/#{planet.id}"
+  end
+
+  def destroy
+    # Find the Planet that I am interested in destroying
+    planet = Planet.find_by(id: params["id"])
+    # Somehow delete it
+    planet.destroy
+    # Redirect back to the list of all planets
+    redirect_to "/planets"
+  end
+
+  private
+    def planet_params
+      # Strong Parameters || Whitelisting
+      # A way to add params directly into the database by describing an appropriate structure
+      params.require(:planet).permit(:name, :image, :orbit, :moons, :diameter)
+    end
 end
--- 08-rails/SolarSystemApp/config/routes.rb
+++ 08-rails/SolarSystemApp/config/routes.rb
@@ -10,7 +10,16 @@ Rails.application.routes.draw do
   # CRUD System for planets
 
   get '/planets' => 'planets#index'
-  get '/planets/new' => 'planets#new'
+
+  get '/planets/new' => 'planets#new' # shows the form
+  post '/planets' => 'planets#create' # handles the submission of the form
+
+
   get '/planets/:id' => 'planets#show'
+
+  get '/planets/:id/edit' => 'planets#edit'
+  post '/planets/:id' => 'planets#update'
+
+  delete '/planets/:id/delete' => 'planets#destroy'
 
 end
