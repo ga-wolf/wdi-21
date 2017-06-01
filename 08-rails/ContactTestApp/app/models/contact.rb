e4dc7b770540686746843cea3225cde0d8dba175
ga-wolf <jack.jeffress@ga.co>
2017-06-01 11:02:14 +1000

    Added instance method test

--- 08-rails/ContactTestApp/app/models/contact.rb
+++ 08-rails/ContactTestApp/app/models/contact.rb
@@ -1,3 +1,19 @@
 class Contact < ApplicationRecord
   validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  # c.print_details()
+    # Bill Murray: bill@ga.co
+    # Groucho Marx: groucho@marx.com
+  def print_details
+    full_name + ": " + email
+    # "#{full_name}: #{email}"
+  end
+
+  def full_name
+    # contact_record.full_name()
+      # self == contact_record
+      # We can access all data on that record
+    first_name + " " + last_name
+  end
 end
--- 08-rails/ContactTestApp/spec/models/contact_spec.rb
+++ 08-rails/ContactTestApp/spec/models/contact_spec.rb
@@ -20,13 +20,34 @@ RSpec.describe Contact, type: :model do
       # The error message for presence is "First name can't be blank"
 
     contact.valid? # Generate error messages for me (if necessary)
-    
+
     expect(contact.errors.full_messages).to include("First name can't be blank")
   end
 
-  it "is invalid without a last_name"
-  it "is invalid without an email"
-  it "is invalid with a duplicate email address"
-  it "should return a full name as a string"
+  # it "is invalid without a last_name"
+
+  it "is invalid without an email" do
+    # Create a new contact, making sure it doesn't have an email
+    contact = Contact.new first_name: "Bill", last_name: "Murray", email: nil
+    # Check to see if the contact is valid (this will generate error messages, if any)
+    contact.valid?
+    # Make sure that an error message exists for email
+    error_messages = contact.errors.full_messages
+    expect( error_messages ).to include("Email can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    Contact.create first_name: "Bill", last_name: "Murray", email: "bill@ga.co"
+    c2 = Contact.new first_name: "B", last_name: "M", email: "bill@ga.co"
+    c2.valid? # Generate error messages (if necessary)
+    errors = c2.errors.full_messages
+    expect(errors).to include("Email has already been taken")
+  end
+
+  it "should return a full name as a string" do
+    contact = Contact.create first_name: "Bill", last_name: "Murray", email: "bill@ga.co"
+
+    expect( contact.full_name() ).to eq("Bill Murray")
+  end
 
 end
